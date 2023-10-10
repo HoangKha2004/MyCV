6adbd2b0083b85887a16072eae5f9f3aac82bc30
HoangKha2004 <khath2004@gmail.com>
2023-10-10 18:04:30 +0700

    change

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 var typed = new Typed(".text", {
-  strings: ["Frontend Developer", "Web Developer"],
-  typeSpeed: 50,
-  backSpeed: 50,
-  backDelay: 1000,
-  loop: true
+    strings: ["Frontend Developer", "Web Developer", "UI/UX Developer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
+
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+const tabs = $$('.header-item-link');
+const tabActive = $('.header-item-link.header-item--active');
+const line = $(".line");
+line.style.left = tabActive.offsetLeft + 'px'
+line.style.width = tabActive.offsetWidth + 'px'
+tabs.forEach((tab, index) => {
+  tab.onclick = function() {
+    $('.header-item-link.header-item--active').classList.remove('header-item--active')
+    this.classList.add('header-item--active')
+    line.style.left = this.offsetLeft + 'px'
+    line.style.width = this.offsetWidth + 'px'
+  }
 });
